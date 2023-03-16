e899dc1c479a80de4f087ded1389a02858e396ef
Gigi Jishkariani <73842838+Jishkarianii@users.noreply.github.com>
2023-03-16 16:05:09 +0400

    Make responsive

--- src/components/NoHoverSlider/index.jsx
+++ src/components/NoHoverSlider/index.jsx
@@ -10,6 +10,12 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper";
 import NoHoverSliderItem from "../NoHoverSliderItem";
+import { useEffect, useState } from "react";
+
+function resizeWindow() {
+  const width = window.innerWidth;
+  return width < 600;
+}
 
 function NoHoverSlider({
   title,
@@ -18,29 +24,57 @@ function NoHoverSlider({
   slidesPerView,
   navigation = true,
 }) {
+  const [respView, setRespView] = useState(resizeWindow());
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => window.addEventListener("resize", resizeHandler);
+  }, []);
+
+  const resizeHandler = () => {
+    setRespView(resizeWindow());
+  };
+
   return (
     <div data-aos="fade-up">
-      <Swiper
-        className="no-hover-slider"
-        spaceBetween={20}
-        slidesPerView={slidesPerView}
-        simulateTouch={navigation}
-        speed={500}
-        navigation={navigation}
-        modules={[Navigation]}
-      >
-        <div className={`head ${!navigation ? "full" : ""}`}>
-          <h3>{title}</h3>
-          {subText && <p>{subText}</p>}
+      {!respView ? (
+        <Swiper
+          className="no-hover-slider"
+          spaceBetween={20}
+          slidesPerView={slidesPerView}
+          simulateTouch={navigation}
+          speed={500}
+          navigation={navigation}
+          modules={[Navigation]}
+        >
+          <div className={`head ${!navigation ? "full" : ""}`}>
+            <h3>{title}</h3>
+            {subText && <p>{subText}</p>}
+          </div>
+          {data.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <div data-aos="fade-up" data-aos-delay={idx * 200}>
+                <NoHoverSliderItem img={item.img} title={item.title} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className="mobile-slider">
+          <div className="head">
+            <h3>{title}</h3>
+          </div>
+          <div className="slider-cont">
+            {data.map((item, idx) => (
+              <div className="slide" key={idx}>
+                <div data-aos="fade-up" data-aos-delay={idx * 200}>
+                  <NoHoverSliderItem img={item.img} title={item.title} />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        {data.map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <div data-aos="fade-up" data-aos-delay={idx * 200}>
-              <NoHoverSliderItem img={item.img} title={item.title} />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      )}
     </div>
   );
 }
--- src/components/PromoSlider/index.jsx
+++ src/components/PromoSlider/index.jsx
@@ -6,30 +6,54 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // import required modules
-import { Navigation } from "swiper";
+import { Navigation, Pagination } from "swiper";
+import { useEffect, useState } from "react";
+
+function resizeWindow() {
+  const width = window.innerWidth;
+  return width < 1020;
+}
 
 function PromoSlider({ data }) {
+  const [respView, setRespView] = useState(resizeWindow());
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => window.addEventListener("resize", resizeHandler);
+  }, []);
+
+  const resizeHandler = () => {
+    setRespView(resizeWindow());
+  };
+
   return (
     <div data-aos="fade">
       <Swiper
-        className="promo-slider"
+        className={`promo-slider ${respView ? "mobile" : ""}`}
         simulateTouch={false}
         speed={500}
         loop={true}
-        navigation={true}
-        modules={[Navigation]}
+        navigation={!respView}
+        pagination={respView}
+        spaceBetween={respView ? 20 : 0}
+        modules={[Navigation, Pagination]}
       >
         {data.map((item, idx) => (
           <SwiperSlide key={idx}>
             <img src={item.img} alt="promo" />
           </SwiperSlide>
         ))}
-        <svg className="curve" viewBox="0 0 180 72">
-          <path d="M180,0v72H0c16.6,0,30-13.4,30-30c0-8.3,3.3-15.8,8.8-21.2C44.2,15.3,51.7,12,60,12h108C174.6,12,180,6.6,180,0z" />
-        </svg>
-        <div className="dot"></div>
+        {!respView && (
+          <>
+            <svg className="curve" viewBox="0 0 180 72">
+              <path d="M180,0v72H0c16.6,0,30-13.4,30-30c0-8.3,3.3-15.8,8.8-21.2C44.2,15.3,51.7,12,60,12h108C174.6,12,180,6.6,180,0z" />
+            </svg>
+            <div className="dot"></div>
+          </>
+        )}
       </Swiper>
     </div>
   );
